c058e9d6557d23c700f6ac685ba7f5bb1850c3e4
the-manpreet-singh <manpreet.singh10196@gmail.com>
2021-12-13 15:57:41 +0530

    enzyme setup complete

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import React from "react";
-import Enzyme from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 
 Enzyme.configure({ adaptor: new EnzymeAdapter() });
 
-test("renders learn react link", () => {});
+test("renders App component", () => {
+  const wrapper = shallow(<App />);
+  console.log(wrapper.debug());
+});
