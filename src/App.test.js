3e8f59aa5533041f36f42de82fa9a242a426401f
the-manpreet-singh <manpreet.singh10196@gmail.com>
2021-12-13 15:20:54 +0530

    enzyme setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import Enzyme from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText("Hello World");
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adaptor: new EnzymeAdapter() });
+
+test("renders learn react link", () => {});
