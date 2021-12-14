b9cec83e64ae69c280bd434432d5770b8dd2cfc9
the-manpreet-singh <manpreet.singh10196@gmail.com>
2021-12-14 17:04:58 +0530

    add some test with data-set attr

--- src/App.test.js
+++ src/App.test.js
@@ -5,25 +5,24 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-
 test("renders without error", () => {
-const wrapper = shallow(<App/>);
-const appComponent = wrapper.find("[data-test='component-app']");
-expect(appComponent.length).toBe(1);
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
 
 test("renders increment button", () => {
-  
+  const wrapper = shallow(<App />);
+  const button = wrapper.find("[data-test='increment-button']");
+  expect(button.length).toBe(1);
 });
 
 test("renders counter display", () => {
-  
+  const wrapper = shallow(<App />);
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
 });
 
-test("counter starts at 0", () => {
-  
-});
+test("counter starts at 0", () => {});
 
-test("clicking button increments counter display", () => {
-
-});
+test("clicking button increments counter display", () => {});
